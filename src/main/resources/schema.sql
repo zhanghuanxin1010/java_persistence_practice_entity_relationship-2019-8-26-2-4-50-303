e4474c39ce0c1e7139d9565ff56a4a79846352b4
unknown <1012151538@qq.comgit config --global user.name >
2019-08-29 19:29:28 +0800

    database

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,35 @@
 CREATE TABLE employee (
   id INTEGER PRIMARY KEY,
   name VARCHAR(64) NOT NULL,
-  age   int(4) NOT NULL
+  age  INT(4) NOT NULL
 );
+
+
+CREATE TABLE systemAdministator (
+  sysAdmId INTEGER PRIMARY KEY,
+  userName VARCHAR(64) NOT NULL,
+  password  INT(4) NOT NULL
+);
+
+CREATE TABLE company (
+  companyId INTEGER PRIMARY KEY,
+  companyAdminId INTEGER NOT NULL,
+  companyName  VARCHAR(16) NOT NULL,
+  sysAdmID INTEGER,
+  FOREING key(companyAdminId) REFERENCES companyAdministator(companyAdminId),
+  
+);
+
+CREATE TABLE employee (
+ employeeId INTEGER PRIMARY KEY,
+  companyId INTEGER NOT NULL, 
+  FOREING key(companyId) REFERENCES company(companyId)
+);
+CREATE TABLE companyAdministator (
+ companyAdminId INTEGER PRIMARY KEY,
+  companyAdminName VARCHAR(16) NOT NULL,
+  employeeId INTEGER  NOT NULL,
+  FOREING key(employeeId) REFERENCES employee(employeeId)
+  
+);
+
